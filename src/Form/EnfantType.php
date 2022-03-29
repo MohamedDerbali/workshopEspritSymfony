aa33b7043a92a773de9faa3a7848069108fe1386
mohamedderbali <mohamed.derbali@esprit.tn>
2022-03-29 16:22:01 +0200

    first commit

---
+++ src/Form/EnfantType.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Enfant;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class EnfantType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('addresse')
+            ->add('dateNais')
+            ->add('gender')
+            ->add('nom')
+            ->add('prenom')
+          //  ->add('parentsIdUser')
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Enfant::class,
+        ]);
+    }
+}
